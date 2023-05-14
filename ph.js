2639c3d3212fb7433484ab94a8c72be8896846ec
TheMogli <d.lampricht@gmx.de>
2023-05-14 23:48:29 +0200

    c

--- ph.js
+++ ph.js
@@ -6,3 +6,21 @@ window.addEventListener("DOMContentLoaded", function() {
     // Update dynamic text
     dynamicTextElement.textContent = "This is dynamically generated text!";
   });
+  
+  function handleImageClick() {
+    alert("Image clicked!");
+  }
+  
+  function checkInputs() {
+    var input1 = document.getElementById("input1").value;
+    var input2 = document.getElementById("input2").value;
+    var input3 = document.getElementById("input3").value;
+    
+    // Check if all inputs are correct
+    if (input1 === "Correct" && input2 === "Correct" && input3 === "Correct") {
+      alert("All inputs are correct!");
+    } else {
+      alert("Some inputs are incorrect. Please try again.");
+    }
+  }
+  
